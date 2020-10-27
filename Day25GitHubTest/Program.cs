38dc28bc21af1e1ba930dcd1fecf222ac7e373bb
ZhouX <ZhouX@DESKTOP-0D1LUA9>
2020-10-27 16:58:50 +0800

    test submit

---
+++ Day25GitHubTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day25GitHubTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+            animal.Car();
+            Console.WriteLine("Hello World");
+            Console.Read();
+        }
+    }
+
+    class Animal
+    {
+        public void Car()
+        {
+            Console.WriteLine("喵喵喵");
+        }
+    }
+    
+}
